07dd46da839d4d9d17784dc4656760507d572930
OKJXI444 <46093931+OKJXI444@users.noreply.github.com>
2019-02-21 14:03:33 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("548126311969980431")
+setInterval(function() {
+channel.send(`هل انت رجل ؟ هل انت بنت ؟`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
